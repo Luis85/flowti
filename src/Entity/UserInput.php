f46e85159fd43877344b7c83695e39d31f8385f9
Luis <mendezluis0815@gmail.com>
2022-12-30 20:11:06 +0100

    added toString function to UserInput.php

--- src/Entity/UserInput.php
+++ src/Entity/UserInput.php
@@ -7,6 +7,11 @@ class UserInput
 
     protected string $text;
 
+    public function __toString(): string
+    {
+        return $this->text;
+    }
+
     public function setText(string $text): void
     {
         $this->text = $text;
