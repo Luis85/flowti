a3510f4559564700544894c3ada9f875025f9517
Luis <mendezluis0815@gmail.com>
2022-12-30 18:54:18 +0100

    added UserInputForm to the Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\UserInput;
+use App\Form\UserInputType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $userInput = new UserInput();
+
+        $form = $this->createForm(UserInputType::class, $userInput);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$userInput` variable has also been updated
+            $userInput = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form,
         ]);
     }
 }
