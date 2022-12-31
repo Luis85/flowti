2c47d102fbf96c493acef0ea0f30825d7677ca78
Luis <mendezluis0815@gmail.com>
2023-01-01 00:46:15 +0100

    inverted data findAll to get the current entries first

--- src/Repository/DataRepository.php
+++ src/Repository/DataRepository.php
@@ -11,7 +11,6 @@ use Doctrine\Persistence\ManagerRegistry;
  *
  * @method Data|null find($id, $lockMode = null, $lockVersion = null)
  * @method Data|null findOneBy(array $criteria, array $orderBy = null)
- * @method Data[]    findAll()
  * @method Data[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class DataRepository extends ServiceEntityRepository
@@ -19,6 +18,11 @@ class DataRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Data::class);
+    }
+
+    public function findAll(): array
+    {
+        return $this->findBy([], ['id' => 'DESC']);
     }
 
     public function save(Data $entity, bool $flush = false): void
