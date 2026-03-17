bbc94a5e3e127dc783bdb8b1ef165dc319bd3cdb
Luis Mendez <hallo@luis-mendez.de>
2026-03-17 23:48:29 +0100

    testing around

--- 01 - Projects/Flowti CLI/components/.storybook/main.ts
+++ 01 - Projects/Flowti CLI/components/.storybook/main.ts
@@ -1,11 +1,18 @@
 import type { StorybookConfig } from "@storybook/html-vite";
 
 const config: StorybookConfig = {
-	stories: ["../**/*.stories.ts"],
-	framework: "@storybook/html-vite",
-	core: {
+    stories: ["../**/*.stories.ts"],
+    framework: "@storybook/html-vite",
+
+    core: {
 		disableTelemetry: true,
 	},
+
+    addons: [
+        "@storybook/addon-vitest",
+        "@storybook/addon-docs",
+        "@storybook/addon-a11y"
+    ]
 };
 
 export default config;
